44121d40e68cf00cb7add55dc36a1d1874fbb702
hydrexao <martin.rambault@eleves.ec-nantes.fr>
2019-03-01 10:38:25 +0100

    grab

--- Projet_Unity/Assets/Scripts/Grab.cs
+++ Projet_Unity/Assets/Scripts/Grab.cs
@@ -1,18 +1,56 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
+[RequireComponent(typeof(HandUtils))]
 public class Grab : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+
+    private HandUtils hand;
+    private bool grabbed;
+    private bool isGrabbing;
+
+    public bool Pinch
     {
-        
+        get
+        {
+            return hand.Pinch;
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Awake()
     {
-        
+        hand = GetComponent<HandUtils>();
+
+        grabbed = false;
+        isGrabbing = false;
+    }
+
+    private void Update()
+    {
+        if (!grabbed && hand.Pinch)
+        {
+            grabbed = true;
+        }
+        else if (grabbed && !hand.Pinch)
+        {
+            grabbed = false;
+            isGrabbing = false;
+        }
+
+    }
+
+    protected void OnCollisionEnter(Collision collision)
+    {
+        if (grabbed && ! isGrabbing)
+        {
+            collision.gameObject.transform.parent = transform;
+            isGrabbing = true;
+
+            Rigidbody rigid_body = collision.gameObject.GetComponent<Rigidbody>();
+
+            if (rigid_body != null)
+            {
+                rigid_body.isKinematic = true;
+            }
+        }
     }
 }
